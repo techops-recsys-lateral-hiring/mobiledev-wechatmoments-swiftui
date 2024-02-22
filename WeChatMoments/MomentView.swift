c9480e84584b002c654636fbb87fdf88d8ad639f
Tushar Sharma <tushar.sharma@thoughtworks.com>
2024-02-22 14:15:14 +0530

    Created empty SwiftUI project with .gitignore and README.md file

---
+++ WeChatMoments/MomentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeChatMoments
+//
+//  Created by Tushar Sharma on 22/02/24.
+//
+
+import SwiftUI
+
+struct MomentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MomentView()
+}
---
+++ WeChatMoments/WeChatMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeChatMomentsApp.swift
+//  WeChatMoments
+//
+//  Created by Tushar Sharma on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeChatMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
