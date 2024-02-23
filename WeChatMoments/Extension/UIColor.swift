4df370e97a8bfd6944f9a5d25dafd44cc5e7190a
Nontapat Siengsanor <nontapat.siengsanor@thoughtworks.com>
2024-02-24 01:37:42 +0700

    Eliminate dependencies, refactor and remove unused code

--- WeChatMoments/Extension/UIColor.swift
+++ WeChatMoments/Extension/UIColor.swift
@@ -5,10 +5,8 @@
 //  Created by nontapat.siengsanor on 23/2/24.
 //
 
-import UIKit
+import SwiftUI
 
-extension UIColor {
-    class func commentsBackgroudColor() -> UIColor {
-        return UIColor(red: 231 / 255, green: 231 / 255, blue: 231 / 255, alpha: 1)
-    }
+extension Color {
+    static let commentsBackgroudColor = Color.gray
 }
