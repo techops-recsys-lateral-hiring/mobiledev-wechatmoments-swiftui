23c4f092797f2e5c1e39a96b7e8d53e8c25d3536
Wolé Solana <157542379+osolana-tw@users.noreply.github.com>
2024-03-05 16:32:37 -0600

    Add helper methods for creating image grid from tweet content

--- WeChatMoments/View/TweetView.swift
+++ WeChatMoments/View/TweetView.swift
@@ -9,27 +9,35 @@ import SwiftUI
 
 struct TweetView: View {
     var tweet: Tweet
+    private let imageLimit = 5
+    private let gridColumnCount = 3
 
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
+                // TODO: Use correct tweet sender avatar
                 avatar(_from: nil)
                     .frame(
                         width: Constants.SENDER_AVATAR_SIZE.width,
                         height: Constants.SENDER_AVATAR_SIZE.height)
                     .cornerRadius(5.0)
 
+                // TODO: Assign the correct user name or nickname
                 Text("Placeholder Profile")
                     .font(.system(size: Constants.FONT_SIZE_CONTENT))
                     .foregroundColor(.blue)
                     .padding(.leading, Constants.TWEET_SENDER_LEFT_OFFSET)
                     .frame(maxWidth: 100)
             }
+            Text(tweet.content ?? "")
+                .foregroundColor(.black)
+                .font(.system(size: Constants.FONT_SIZE_CONTENT))
+                .lineLimit(nil)
+                .padding(.top, Constants.TWEET_CONTENT_TOP_OFFSET)
+
+            // TODO: Add tweet image grid to view
         }
-        Text(tweet.content ?? "")
-            .foregroundColor(.black)
-            .font(.system(size: Constants.FONT_SIZE_CONTENT))
-            .lineLimit(nil)
+        .padding(.all)
     }
 
     private func avatar(_from avatarURL: Any?) -> Image {
@@ -43,8 +51,46 @@ struct TweetView: View {
         return avatar
     }
 
+    private func addImagesToView(_from URLs: [String]?) -> (any View)? {
+        guard let imageURLs = URLs else { return nil }
+        var images: [Image] = []
+
+        for imageURL in imageURLs {
+            guard let image = fetchImage(_from: imageURL) else { return nil }
+            images.append(image)
+        }
+
+
+        return LazyVGrid(columns: Array(repeating: GridItem(), count: gridColumnCount),
+                         spacing: Constants.TWEET_IMAGE_SEPARATOR_SPACE) {
+            ForEach(0..<imageLimit, id: \.self) { index in
+                if index < images.count {
+                    images[index]
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: Constants.IMAGE_SIZE.width * 2,
+                               height: Constants.IMAGE_SIZE.height * 2)
+                        .clipped()
+                } else {
+                    // Placeholder view for empty slots in the grid
+                    Color.clear
+                }
+            }
+        }
+                         .padding()
+    }
+
+    private func fetchImage(_from URL: String) -> Image? {
+        var imageToReturn: Image? = nil
+        ImageHelper.shared.getImage(URL, forSize: Constants.IMAGE_SIZE) { image in
+            imageToReturn = Image(uiImage: image!)
+        }
+
+        return imageToReturn
+    }
+
 }
 
 #Preview {
-    TweetView(tweet: Tweet(content: "This is a test tweet"))
+    TweetView(tweet: Tweet(content: "Unlike many proprietary big data processing platform different, Spark is built on a unified abstract RDD, making it possible to deal with different ways consistent with large data processing scenarios, including MapReduce, Streaming, SQL, Machine Learning and Graph so on. To understand the Spark, you have to understand the RDD."))
 }
